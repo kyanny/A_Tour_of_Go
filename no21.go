196010abbb00a0d2ab6f4f9fdc4efdd89e098f5d
Kensuke Nagae <kyanny@gmail.com>
2012-10-21 02:58:54 +0900

    Add #21

---
+++ no21.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10), // あーそうか、こっちは else で else では return してないので、 >= の printf が印字されたあと、次の行で lim も印字されるのか。
+		pow(3, 3, 20),
+		 )
+}
