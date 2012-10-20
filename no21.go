ebc2f787ff009e962dd80853907505c299b5b8e3
Kensuke Nagae <kyanny@gmail.com>
2012-10-21 03:00:21 +0900

    実験

--- no21.go
+++ no21.go
@@ -19,4 +19,9 @@ func main() {
 		pow(3, 2, 10), // あーそうか、こっちは else で else では return してないので、 >= の printf が印字されたあと、次の行で lim も印字されるのか。
 		pow(3, 3, 20),
 		 )
+
+	fmt.Println("----")
+	fmt.Println(pow(3, 2, 10)) // 9
+	fmt.Println("----")
+	fmt.Println(pow(3, 3, 20)) // 27 >= 20\n20
 }
