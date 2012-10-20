aacd9075323fd7496449dd32dc0b7db086939aa4
Kensuke Nagae <kyanny@gmail.com>
2012-10-21 02:35:01 +0900

    Comment

--- no10.go
+++ no10.go
@@ -2,8 +2,9 @@ package main
 
 import "fmt"
 
-var x, y, z int = 1, 2, 3
-var c, python, java = true, false, "no!"
+var x, y, z int = 1, 2, 3 // 型宣言つき
+var c, python, java = true, false, "no!" // 型宣言省略できる
+// var foo, bar int, baz string = 1, 2, "3" // こういうふうには書けない
 
 func main() {
 	fmt.Println(x, y, z, c, python, java)
