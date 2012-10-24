442e051c834233932a651dbdc59e6566e016d521
Kensuke Nagae <kyanny@gmail.com>
2012-10-25 02:16:58 +0900

    Add comment

--- no39.go
+++ no39.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	pow := make([]int, 10) // 復習: これは slice を作ってて要素上限が 10 個だっけ?
-	for i := range pow {
+  for i := range pow { // _, value でうけなければインデックスだけ得られる
 		pow[i] = 1<<uint(i)
 	}
-	for _, value := range pow {
+	for _, value := range pow { // _ で受けることで値を捨てる (_ が予約語的なものなのか、単に不要な変数を受ける仮の名前というイディオムなのか?)
 		fmt.Printf("%d\n", value)
 	}
 }
