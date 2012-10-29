1f77ef7d5a01aabc2f468303e1d1545f5741783e
Kensuke Nagae <kyanny@gmail.com>
2012-10-30 03:24:57 +0900

    演習問題 #43 の改良版を実装した。
    math.Sqrt(x) の計算結果と差をとり絶対値が 0.01 以下だったらそこで計算をやめて値を返すようにした。それによると x が小さいとループの回数が少なくても平方根とかなり近似の値になり、 x が大きくなるにつれて近似するまでにかかる計算回数が増えるようだった (これが正しいのかは知らない) 結局 x = 7 の時点で 10 回の計算では十分な近似値が得られなくなった。

--- no43.go
+++ no43.go
@@ -5,19 +5,40 @@ import (
 	"math"
 )
 
-func Sqrt(x float64) float64 {
+func Sqrt(x float64) (float64, int) {
 	z := float64(0)
 	for i := 0; i < 10; i++ {
 		z = z - ( (math.Pow(z, 2) - x) / math.Pow(2, z))
+		if math.Abs(math.Sqrt(x) - z) < 0.01 {
+			return z, i
+		}
 	}
-	return z
+	return z, 10
 }
 
 func main() {
-	fmt.Println(Sqrt(1))
-	fmt.Println(math.Sqrt(1))
-	fmt.Println(Sqrt(2))
-	fmt.Println(math.Sqrt(2))
-	fmt.Println(Sqrt(3))
-	fmt.Println(math.Sqrt(3))
+	z1, i1 := Sqrt(1)
+	fmt.Printf("z = %f (loop#%d)\n", z1, i1)
+	fmt.Printf("math.Sqrt(1) = %f\n", math.Sqrt(1))
+	fmt.Printf("delta = %f\n\n", math.Abs(z1 - math.Sqrt(1)))
+
+	z2, i2 := Sqrt(2)
+	fmt.Printf("z = %f (loop#%d)\n", z2, i2)
+	fmt.Printf("math.Sqrt(2) = %f\n", math.Sqrt(2))
+	fmt.Printf("delta = %f\n\n", math.Abs(z2 - math.Sqrt(2)))
+
+	z3, i3 := Sqrt(3)
+	fmt.Printf("z = %f (loop#%d)\n", z3, i3)
+	fmt.Printf("math.Sqrt(3) = %f\n", math.Sqrt(3))
+	fmt.Printf("delta = %f\n\n", math.Abs(z3 - math.Sqrt(3)))
+
+	z5, i5 := Sqrt(5)
+	fmt.Printf("z = %f (loop#%d)\n", z5, i5)
+	fmt.Printf("math.Sqrt(5) = %f\n", math.Sqrt(5))
+	fmt.Printf("delta = %f\n\n", math.Abs(z5 - math.Sqrt(5)))
+
+	z7, i7 := Sqrt(7)
+	fmt.Printf("z = %f (loop#%d)\n", z7, i7)
+	fmt.Printf("math.Sqrt(7) = %f\n", math.Sqrt(7))
+	fmt.Printf("delta = %f\n\n", math.Abs(z7 - math.Sqrt(7)))
 }
