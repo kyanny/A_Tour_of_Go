40b5c6c27192d2c215863063ba7face4c5e858cf
Kensuke Nagae <kyanny@gmail.com>
2012-11-02 02:30:33 +0900

    フィボナッチ数 書き直し

--- no46.go
+++ no46.go
@@ -2,25 +2,20 @@ package main
 
 import "fmt"
 
-func fibonacci() func(int) int {
-	var f func(n int) int
-	f = func(n int) int {
-		x := 0
-		if n == 0 {
-			x = 0
-		} else if n == 1 {
-			x = 1
-		} else {
-			x = f(n-1) + f(n-2)
-		}
-		return x
+func fibonacci() func() int {
+	n := 0
+	m := 1
+	return func() int {
+		ret := n
+		n = m
+		m = ret + m
+		return ret
 	}
-	return f
 }
 
 func main() {
 	f := fibonacci()
 	for i := 0; i < 10; i++ {
-		fmt.Println(f(i))
+		fmt.Println(f())
 	}
 }
