6d1202b6230a8aec7cb75227682174c98cabbed5
Kensuke Nagae <kyanny@gmail.com>
2012-11-21 02:07:57 +0900

    ポインタ型じゃなくて値そのものを渡す・受け取る例を追加

--- no51.go
+++ no51.go
@@ -18,8 +18,21 @@ func (v *Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+func (v Vertex) Scale2(f float64) {
+	v.X = v.X * f
+  v.Y = v.Y * f
+}
+
+func (v Vertex) Abs2() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
 func main() {
 	v := &Vertex{3, 4}
 	v.Scale(5)
 	fmt.Println(v, v.Abs())
+
+	v2 := &Vertex{3, 4}
+	v2.Scale2(5)
+	fmt.Println(v2, v2.Abs2())
 }
